18ed65b2a4d961850a316d7c1e29ad73d3c80089
App-Developer <rafiullahkhaleel@gmail.com>
2025-12-01 18:38:49 -0800

    feat: enhance DbHelper to support note creation and database initialization

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -7,14 +7,48 @@ import 'package:sqflite/sqflite.dart';
 class DbHelper {
   DbHelper._();
   static final DbHelper getInstance = DbHelper._();
+  static final tableName = 'note';
+  static final sNo = 'sNo';
+  static final noteTitle = 'title';
+  static final noteDesc = 'desc';
   static Database? _db;
   Future<Database> get database async {
     return _db ?? await initDB();
   }
 
-  Future<Database> initDB() async{
+  Future<Database> initDB() async {
     Directory docDir = await getApplicationDocumentsDirectory();
     String path = join(docDir.path, 'test.db');
-    return openDatabase(path);
+    return openDatabase(
+      path,
+      onCreate: (db, version) {
+        db.execute('''
+        CREATE TABLE $tableName(
+          $sNo INTEGER PRIMARY KEY AUTOINCREMENT,
+          $noteTitle TEXT,
+          $noteDesc TEXT
+        )
+      ''');
+
+        /// you can make multiple tables like that but names of table should be unique
+        // db.execute('''
+        //   CREATE TABLE $anotherName(
+        //     $sNo INTEGER PRIMARY KEY AUTOINCREMENT,
+        //     $noteTitle TEXT,
+        //     $noteDesc TEXT
+        //   )
+        // ''');
+      },
+      version: 1,
+    );
+  }
+
+  Future<bool> addNote({required String title, required String desc}) async {
+    final db = await database;
+    int rowsEffected = await db.insert(tableName, {
+      'title': title,
+      'desc': desc,
+    });
+    return rowsEffected > 0;
   }
 }
